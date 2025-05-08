6012f32228bb3067216523814a40851a13ff0421
joseBenites <yopue12@gmail.com>
2025-05-08 11:53:58 -0500

    agregando coockies y arreglando noticia con documentos

--- Backend/routers/jwt_auth.py
+++ Backend/routers/jwt_auth.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Response
 from sqlalchemy.orm import Session
 from fastapi.security import OAuth2PasswordRequestForm
 from passlib.context import CryptContext
@@ -11,6 +11,26 @@ router = APIRouter()
 crypt = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 @router.post("/login")
+def login(response: Response, form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+
+    user = db.query(UserInDb).filter(UserInDb.username == form_data.username).first()
+    if not user or not crypt.verify(form_data.password, user.hashed_password):
+        raise HTTPException(status_code=400, detail="Credenciales incorrectas")
+    if user.disabled:
+        raise HTTPException(status_code=400, detail="Usuario inactivo")
+    ...
+    token = create_access_token(data={"sub": UserInDb.username})
+    response.set_cookie(
+        key="access_token",
+        value=f"Bearer {token}",
+        httponly=True,
+        secure=True,  # importante si usas HTTPS
+        samesite="Lax"
+    )
+    return {"message": "Login exitoso"}
+
+'''
+@router.post("/login")
 def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     user = db.query(UserInDb).filter(UserInDb.username == form_data.username).first()
     if not user or not crypt.verify(form_data.password, user.hashed_password):
@@ -20,3 +40,4 @@ def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depend
 
     token = create_access_token(data={"sub": user.username})
     return {"access_token": token, "token_type": "bearer"}
+'''
