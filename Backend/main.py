4e297a44b553dbced6efdc84cdc91cf5a1f1f3ad
joseBenites <yopue12@gmail.com>
2025-05-08 14:20:23 -0500

    arreglando cookies

--- Backend/main.py
+++ Backend/main.py
@@ -13,7 +13,7 @@ app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # origen de tu frontend
+    allow_origins=["http://9.0.1.247:8090"],  # origen de tu frontend
     allow_credentials=True,
     allow_methods=["*"],  # puedes restringir si quieres
     allow_headers=["*"],  # puedes restringir si quieres
