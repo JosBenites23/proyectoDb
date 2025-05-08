c829b5a40c6ce994a8e471c78fe4d67b07de87f6
joseBenites <yopue12@gmail.com>
2025-05-08 16:42:41 -0500

    arreglando cookies

---
+++ Backend/routers/logout.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter, Response, status
+
+router = APIRouter()
+
+@router.post("/logout", status_code=status.HTTP_200_OK)
+async def logout(response: Response):
+
+    response.delete_cookie(
+        key="access_token",
+        httponly=True,
+        secure=False,  # importante si usas HTTPS
+        samesite="None",  # Permite cookies en solicitudes de origen cruzado
+    )
+    return {"message": "Sesión cerrada correctamente"}
