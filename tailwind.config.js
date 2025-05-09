6a9dd626e56bd5d0927f985be038a852030fa9cb
joseBenites <yopue12@gmail.com>
2025-05-09 13:52:06 -0500

    agregando animaciones

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+import animations from '@midudev/tailwind-animations';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [animations],
+}
+
